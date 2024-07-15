5b532430ebba55f0e7f2376a6205a2238244d44b
Valeria Serci <balerty84@gmail.com>
2024-07-15 18:13:59 +0100

    test passing for Buzz

--- src/main/scala/FizzBuzz.scala
+++ src/main/scala/FizzBuzz.scala
@@ -4,6 +4,8 @@ object FizzBuzz {
       return "FizzBuzz"
     } else if ((number % 3) == 0) {
       return "Fizz"
+    } else if ((number % 5) == 0) {
+      return "Buzz"
     }
     return ""
   }
--- src/test/scala/FizzBuzzTest.scala
+++ src/test/scala/FizzBuzzTest.scala
@@ -7,6 +7,8 @@ class FizzBuzzTest extends AnyFunSuite {
   test("prints Fizz if divisible by 3") {
     assert(FizzBuzz.generate(3) === "Fizz")
   }
-
+  test("prints Buzz if divisible by 5") {
+    assert(FizzBuzz.generate(5) === "Buzz")
+  }
 }
 
